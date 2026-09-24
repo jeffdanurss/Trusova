f6ee96b3e12391d7d028fa864132f93bbd5cd158
jeffdanurss <150809369+jeffdanurss@users.noreply.github.com>
2026-09-23 23:11:55 -0500

    Improve style and footer

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . '/../src/Config/config.php';
+require __DIR__ . '/../src/Core/autoload.php';
+
+$router = new Router();
+
+$router->get('/', function () {
+    (new CatalogController())->index();
+});
+$router->get('/producto/{slug}', function ($params) {
+    (new CatalogController())->show($params);
+});
+$router->get('/carrito', function () {
+    (new CartController())->show();
+});
+$router->post('/carrito/agregar', function () {
+    (new CartController())->add();
+});
+$router->post('/carrito/actualizar', function () {
+    (new CartController())->update();
+});
+$router->post('/carrito/eliminar', function () {
+    (new CartController())->remove();
+});
+
+$router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
--- src/Config/config.php
+++ src/Config/config.php
@@ -5,5 +5,6 @@ define('DB_USER', 'postgres');
 define('DB_PASS', 'moskova1945');
 define('DB_NAME', 'trusova');   
 define('DB_PORT', '5432');
-define('BASE_URL', 'http://localhost:8080');
+define('BASE_URL', 'http://127.0.0.1:8000');
+define('SITE_NAME', 'Trusova Relojes');
 ?>
